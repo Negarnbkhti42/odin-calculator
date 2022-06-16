155ee7a4358c33b07a326dfc344df14653f01195
Negarnbkhti42 <norob35@gmail.com>
2022-06-16 19:34:03 +0430

    append number on click

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-number_buttons = document.querySelectorAll(".btn");
+class Calculator {
+    constructor(calculationText, previousText) {
+        this.calculationTextElement = calculationText;
+        this.previousTextElement = previousText;
+    }
 
-buttons.array.forEach(element => {
-    element.addEventListener("click", function (e) {
+    clear() {
+        this.calculationText = "";
+        this.previousText = "";
+    }
 
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        if (number == "." && this.calculationText.includes(".")) {
+            return;
+        }
+        this.calculationText = this.calculationText.toString() + number.toString();
+    }
+
+    updateDisplay() {
+        this.calculationTextElement.innerHTML = this.calculationText || "0";
+    }
+}
+
+const numbers = document.querySelectorAll("[data-number]");
+const operators = document.querySelectorAll("[data-operation");
+const equal = document.querySelector("[data-equal]");
+const del = document.querySelector("[data-delete]");
+const clear = document.querySelector("[data-clear]");
+
+const calculationText = document.querySelector("[data-calculation]");
+const previousText = document.querySelector("[data-previous]")
+
+const calculator = new Calculator(calculationText, previousText);
+calculator.clear();
+
+numbers.forEach(element => {
+    element.addEventListener("click", () => {
+        calculator.appendNumber(element.getAttribute("data-key"));
+        calculator.updateDisplay();
     });
+});
+
+operators.forEach(element => {
+    element.addEventListener("click", () => { });
 });
 
 window.addEventListener("keyup", function (e) {
