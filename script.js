70e6253b9fe062622035633033ffb8c48c9807a7
Negarnbkhti42 <norob35@gmail.com>
2022-06-16 18:12:07 +0430

    traverse buttons action

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-buttons = document.querySelectorAll(".btn");
+number_buttons = document.querySelectorAll(".btn");
+
+buttons.array.forEach(element => {
+    element.addEventListener("click", function (e) {
+
+    });
+});
+
 window.addEventListener("keyup", function (e) {
     key = this.document.querySelector(`.btn[data-key="${e.key}"]`);
     console.log(key);
