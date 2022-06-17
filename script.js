07490b7f8ea364480073bac13888972249c7ee2f
Negarnbkhti42 <norob35@gmail.com>
2022-06-17 12:02:13 +0430

    execute operation

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 class Calculator {
+    static Operate = {
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => a / b,
+    };
+
     constructor(calculationText, previousText) {
         this.calculationTextElement = calculationText;
         this.previousTextElement = previousText;
@@ -7,6 +14,7 @@ class Calculator {
     clear() {
         this.calculationText = "";
         this.previousText = "";
+        this.operation = "";
     }
 
     delete() {
@@ -20,8 +28,32 @@ class Calculator {
         this.calculationText = this.calculationText.toString() + number.toString();
     }
 
+    setOperation(operation) {
+
+        if (this.operation && this.operation !== "=") {
+            this.calculationText = this.compute();
+        }
+        this.operation = operation;
+        this.previousText = this.calculationText;
+        this.calculationText = "";
+    }
+
+    compute() {
+        if (this.previousText && this.operation) {
+            return Calculator.Operate[this.operation](parseFloat(this.previousText), parseFloat(this.calculationText || 0));
+        }
+    }
+
+    setResult() {
+        let result = this.compute();
+        this.previousText += this.operation + this.calculationText;
+        this.calculationText = result;
+        this.operation = "=";
+    }
+
     updateDisplay() {
         this.calculationTextElement.innerHTML = this.calculationText || "0";
+        this.previousTextElement.innerHTML = this.previousText + this.operation;
     }
 }
 
@@ -45,14 +77,27 @@ numbers.forEach(element => {
 });
 
 operators.forEach(element => {
-    element.addEventListener("click", () => { });
+    element.addEventListener("click", () => {
+        calculator.setOperation(element.getAttribute("data-key"));
+        calculator.updateDisplay();
+    });
 });
+
+equal.addEventListener("click", () => {
+    calculator.setResult();
+    calculator.updateDisplay();
+})
 
 window.addEventListener("keyup", function (e) {
     key = this.document.querySelector(`.btn[data-key="${e.key}"]`);
-    console.log(key);
+    console.log(numbers.values());
 
     if (!key) {
         return;
     }
+
+    // if (numbers.array.includes(key)) {
+    //     calculator.appendNumber(element.getAttribute("data-key"));
+    //     calculator.updateDisplay();
+    // }
 });
